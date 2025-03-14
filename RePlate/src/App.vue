bd67a2fa27644ee0bcc2989b583e21dcfc70109a
Mary Grace <msar0027@student.monash.edu>
2025-03-14 20:56:59 +1100

    changed font and background color

--- RePlate/src/App.vue
+++ RePlate/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  import { RouterLink, RouterView } from "vue-router";
-  import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,9 +11,8 @@
       <HelloWorld msg="RePlate" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Homelalala</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/template">Template</RouterLink>
       </nav>
     </div>
   </header>
@@ -22,66 +21,71 @@
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+.app-container {
+  background-color: white;
+  min-height: 100vh; 
+  width: 100%;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  border-radius: 50%;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
   header {
-    line-height: 1.5;
-    max-height: 100vh;
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
-    display: block;
-    margin: 0 auto 2rem;
-    border-radius: 50%;
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
 
   nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
-
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
-
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
-
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
-
-  nav a:first-of-type {
-    border: 0;
-  }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
-
-    nav {
-      text-align: left;
-      margin-left: -1rem;
-      font-size: 1rem;
-
-      padding: 1rem 0;
-      margin-top: 1rem;
-    }
-  }
+}
 </style>
--- RePlate/src/components/HelloWorld.vue
+++ RePlate/src/components/HelloWorld.vue
@@ -17,7 +17,8 @@ defineProps<{
 
 <style scoped>
 h1 {
-  font-weight: 500;
+  font-family: 'Lato', sans-serif;
+  font-weight: 700;
   font-size: 2.6rem;
   position: relative;
   top: -10px;
