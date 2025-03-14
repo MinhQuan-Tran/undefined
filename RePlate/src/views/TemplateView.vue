498b73b6abfa0210c73f6e3615ed67a7527096a7
Minh Quan Tran <62203526+MinhQuan-Tran@users.noreply.github.com>
2025-03-14 20:26:02 +1100

    Add TemplateView

---
+++ RePlate/src/views/TemplateView.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    name: "TemplateView",
+    components: {}
+  };
+</script>
+
+<template>
+  <div class="template">
+    <h1>Template Page</h1>
+  </div>
+</template>
+
+<style></style>
