f65b7926fb60b3bbbd07ec968d92af5319f85e75
Mary Grace <msar0027@student.monash.edu>
2025-03-14 20:18:13 +1100

    add logo

--- RePlate/src/App.vue
+++ RePlate/src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/replate-logo.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Homelalala</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
@@ -29,6 +29,7 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+  border-radius: 50%;
 }
 
 nav {
