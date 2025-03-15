1832ef54995dbc3978ba2df0d75594c8f7ca6977
jleu0009 <jleu0009@student.monash.edu>
2025-03-15 13:10:54 +1100

    Navigation bar tailwind styling

--- RePlate/src/App.vue
+++ RePlate/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">About</RouterLink>
         <RouterLink to="/login">Log In</RouterLink>
         <RouterLink to="/signup">Sign Up</RouterLink>
+        <RouterLink to="/feed">Feed</RouterLink>
       </nav>
     </div>
 
@@ -44,7 +45,6 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -84,11 +84,10 @@ nav a:first-of-type {
 
   nav {
     text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
-    margin-top: 1rem;
+    /* margin-top: 1rem; */
   }
 }
 </style>
