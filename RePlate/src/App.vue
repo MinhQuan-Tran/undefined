2a048253b898e82653a48e178c2f23bb19201834
jleu0009 <jleu0009@student.monash.edu>
2025-03-15 17:50:16 +1100

    Current Location calculated

--- RePlate/src/App.vue
+++ RePlate/src/App.vue
@@ -1,13 +1,15 @@
 <script lang="ts">
-  import { RouterLink, RouterView } from "vue-router";
-  import HelloWorld from "./components/HelloWorld.vue";
-  import Navbar from "./components/Navbar.vue";
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+import Location from "./components/Location.vue";
 
-  export default {
-    components: {
-      Navbar
-    }
-  };
+export default {
+  components: {
+    Navbar,
+    Location
+  }
+};
 </script>
 
 <template>
@@ -17,9 +19,9 @@
     <div class="wrapper">
       <HelloWorld msg="RePlate" />
 
+
       <nav>
         <RouterLink to="/">About</RouterLink>
-        <!-- <RouterLink to="/about">About</RouterLink> -->
         <RouterLink to="/auth">Login/Signup</RouterLink>
       </nav>
     </div>
@@ -27,73 +29,76 @@
 
   <Navbar v-else></Navbar>
 
+
   <RouterView />
+  <Location />
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.app-container {
+  background-color: white;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  border-radius: 50%;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
   header {
-    line-height: 1.5;
-    max-height: 100vh;
-  }
-  .app-container {
-    background-color: white;
-    min-height: 100vh;
-    width: 100%;
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
-    display: block;
-    margin: 0 auto 2rem;
-    border-radius: 50%;
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
 
   nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
+    text-align: left;
+    font-size: 1rem;
+
+    padding: 1rem 0;
   }
-
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
-
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
-
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
-
-  nav a:first-of-type {
-    border: 0;
-  }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
-
-    nav {
-      text-align: left;
-      font-size: 1rem;
-
-      padding: 1rem 0;
-    }
-  }
+}
 </style>
---
+++ RePlate/src/components/Location.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useGeolocation } from '@vueuse/core'
+
+const { coords, locatedAt, error} = useGeolocation()
+</script>
+
+<template>
+  <div class="display-location">
+    <h1>Your location: </h1>
+    <h2 v-if="coords.latitude && coords.longitude">
+      Latitude: {{coords.latitude}}
+      <br>
+      Longitude: {{coords.longitude}}
+    </h2>
+
+    <h2 v-else>
+      Locating...
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+.display-location {
+  margin: 20px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+}
+</style>
